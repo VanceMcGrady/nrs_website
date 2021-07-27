75a529c06cdde937f9656c7fb23e6bf50d614756
Vance McGrady <vancemcgrady@Vances-MacBook-Pro.local>
2021-07-27 16:54:23 -0700

    got the random color generator working

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const mainContainer = document.querySelector(".main-container");
 const coloredSquare = document.querySelector(".colored-square");
+const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+for (i = 0; i < 10; i++) {
+  coloredSquare.style.backgroundColor = "#" + randomColor;
+  return coloredSquare;
+}
+
+console.log(coloredSquare);
