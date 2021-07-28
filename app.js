be192d903864fef95e4980831c097bedc5ba64a4
Vance McGrady <vancemcgrady@Vances-MacBook-Pro.local>
2021-07-27 18:04:29 -0700

    Id like to go back to the way things were

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const mainContainer = document.querySelector(".main-container");
-const coloredSquare = document.querySelector(".colored-square");
-const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+let mainContainer = document.getElementById("main-container");
+let coloredSquare = document.getElementById("colored-square");
+let randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
-for (i = 0; i < 10; i++) {
-  coloredSquare.style.backgroundColor = "#" + randomColor;
-}
-
-console.log(coloredSquare);
+window.addEventListener("DOMContentLoaded", function () {
+  return coloredSquare;
+});
