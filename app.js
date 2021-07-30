5eda52662aa60ba34e11df3c4b29992a355b3f2d
Vance McGrady <vancemcgrady@Vances-MacBook-Pro.local>
2021-07-29 19:48:07 -0700

    in css changed black square position to absolute.

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ function createColoredSquares(numOfSquares) {
   function randomColor() {
     return Math.floor(Math.random() * 16777215).toString(16);
   }
-  const colorSquare = document.querySelector(".colored-square");
 
-  console.log(colorSquare);
+  const colorSquare = document.querySelector(".colored-square");
+  const blackSquare = document.querySelector(".black-squares");
+
   for (i = 0; i < numOfSquares; i++) {
     mainContainer.innerHTML += `<div class="colored-square" style="background-color: ${
       "#" + randomColor()
